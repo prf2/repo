7bdf511947f71f1dc8274dd84601505d7d23f7fd
prf2 <rayflix@laposte.net>
2022-12-22 15:54:56 +0100

    maj

--- repo/plugin.video.youtube/resources/lib/youtube_plugin/youtube/helper/video_info.py
+++ repo/plugin.video.youtube/resources/lib/youtube_plugin/youtube/helper/video_info.py
@@ -740,8 +740,10 @@ class VideoInfo(object):
         #                                   'hl': self.language}}}
 
         payload = {'videoId': video_id,
-                   'context': {'client': {'clientVersion': '16.05', 'gl': self.region,
-                                          'clientName': 'ANDROID', 'hl': self.language}}}
+                   'context': {'client': {'clientVersion': '16.49', 'gl': self.region,
+                                          'clientName': 'ANDROID', 'hl': self.language}},
+                   'thirdParty': {'embedUrl': 'https://google.com'}
+        }
 
         player_response = {}
         for attempt in range(2):
@@ -751,9 +753,10 @@ class VideoInfo(object):
                                   allow_redirects=True)
                 r.raise_for_status()
                 player_response = r.json()
-                if player_response.get('playabilityStatus', {}).get('status', 'OK') == 'AGE_CHECK_REQUIRED' \
-                        and attempt == 0:
-                    payload['context']['client']['clientScreen'] = 'EMBED'
+                if player_response.get('playabilityStatus', {}).get('status', 'OK') in \
+                        ('AGE_CHECK_REQUIRED', 'UNPLAYABLE') and attempt == 0:
+                    payload['context']['client']['clientName'] = 'ANDROID_EMBEDDED_PLAYER'
+                    payload['context']['client']['clientVersion'] = '16.20'
                     continue
             except:
                 error_message = 'Failed to get player response for video_id "%s"' % video_id
@@ -863,10 +866,11 @@ class VideoInfo(object):
                 if not reason:
                     reason = 'UNKNOWN'
 
-                try:
-                    reason = reason.encode('raw_unicode_escape').decode('utf-8')
-                except:
-                    pass
+                if PY2:
+                    try:
+                        reason = reason.encode('raw_unicode_escape').decode('utf-8')
+                    except:
+                        pass
 
                 raise YouTubeException(reason)
 
