8b66cda1c55b0827f4dee3002f0f9704c0b8e12b
prf2 <rayflix@laposte.net>
2023-06-19 15:03:14 +0200

    test

--- repo/plugin.video.catchuptvandmore/resources/lib/channels/fr/lequipe.py
+++ repo/plugin.video.catchuptvandmore/resources/lib/channels/fr/lequipe.py
@@ -20,9 +20,11 @@ import xbmcgui
 # Rework Date/AIred
 URL_ROOT = 'https://www.lequipe.fr'
 
-URL_LIVE = URL_ROOT + '/lachainelequipe/'
+URL_LIVE = URL_ROOT + '/tv'
 
 URL_API_LEQUIPE = URL_ROOT + '/equipehd/applis/filtres/videosfiltres.json'
+
+GENERIC_HEADERS = {'User-Agent': web_utils.get_random_ua()}
 
 
 @Route.register
@@ -85,29 +87,29 @@ def get_video_url(plugin, item_id, video_id, download_mode=False, **kwargs):
 
 @Resolver.register
 def get_live_url(plugin, item_id, **kwargs):
+    resp = urlquick.get(URL_LIVE, headers=GENERIC_HEADERS, max_age=-1)
+    root = resp.parse()
+
+    list_url = []
+    list_channel = []
+
+    try:
+        video_list = root.find('.//div[@class="RemoteVideoListWidget"]')
+        for link in video_list.findall('.//a[@class="Link"]'):
+            url = re.compile(r'live\/(.*?)$').findall(link.get('href'))[0]
+            channel = link.find('.//img').get('alt')
+            list_url.append(url)
+            list_channel.append(channel)
+
+    except Exception:
+        list_channel.append("La chaine l'Équipe")
+        list_url.append('x2lefik')
 
     if item_id == 'lequipelive':
-        resp = urlquick.get("https://www.lequipe.fr/directs", headers={'user-agent': web_utils.get_random_ua()}, max_age=-1)
-        live_id = re.compile(r'<article.+?<a href="(.+?)".+?alt="(.+?)"').findall(resp.text)
-        list_url = []
-        list_q = []
-
-        for a in live_id:
-            list_url.append(a[0])
-            list_q.append(a[1])
-
-        if len(list_url) == 0:
-            plugin.notify(plugin.localize(30718), '')
-            return False
-
-        ret = xbmcgui.Dialog().select(Script.localize(30174), list_q)
-        if ret > -1:
-            live_id = list_url[ret]
-        resp = urlquick.get(live_id, headers={'user-agent': web_utils.get_random_ua()}, max_age=-1)
-        live_id = re.compile(r'"EmbedUrl": "(.+?)",').findall(resp.text)[0].rsplit('/', 1)[-1]
-
+        ret = xbmcgui.Dialog().select(Script.localize(30174), list_channel)
     else:
-        resp = urlquick.get(URL_LIVE, headers={'User-Agent': web_utils.get_random_ua()}, max_age=-1)
-        live_id = re.compile(r'video-id\=\"(.*?)\"', re.DOTALL).findall(resp.text)[0]
+        ret = 0
+
+    live_id = list_url[ret]
 
     return resolver_proxy.get_stream_dailymotion(plugin, live_id, False)
